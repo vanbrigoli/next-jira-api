201c2305b9d1a7646b0d5976374efbb079213f20
Van Brigoli <v.brigoli@arcanys.com>
2019-06-13 18:38:16 +0800

    add middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
+import expressJwt from "express-jwt";
+
 import AuthRoutes from "./authRoutes";
 import UserRoutes from "./userRoutes";
+import key from "../config/key";
+import { unAuthorizedRequest } from "../utils/commonResponse";
 
 const routes = router => {
+  router.use(
+    expressJwt({ secret: key.secretKey }).unless({
+      path: ["/api/authenticate"]
+    })
+  );
+
+  router.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+      unAuthorizedRequest(res, { message: "Invalid token." });
+    }
+  });
+
   AuthRoutes(router);
   UserRoutes(router);
   router.route("/").get((req, res) => {
