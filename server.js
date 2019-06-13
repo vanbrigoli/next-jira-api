e4bd39e55943870d7a20197839b168b10237863e
Van Brigoli <v.brigoli@arcanys.com>
2019-06-13 10:42:34 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
