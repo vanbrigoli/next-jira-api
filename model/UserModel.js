d639ee657b66511ccf7fb2df6c92c2b032809d48
Van Brigoli <v.brigoli@arcanys.com>
2019-06-18 09:56:07 +0800

    add image

--- model/UserModel.js
+++ model/UserModel.js
@@ -7,6 +7,7 @@ const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   position: String,
+  image: String,
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
