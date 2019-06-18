71ff5917168f441b677bec5ca79b04879b7ecace
Van Brigoli <v.brigoli@arcanys.com>
2019-06-18 17:18:26 +0800

    set body parser limit to 2mb

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const router = express.Router();
 app.use(cors());
 
 // Setup body parser
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 routes(router);
