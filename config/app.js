25fcf572c926bca2b5fc775c9352c02d02fd2989
Van Brigoli <v.brigoli@arcanys.com>
2019-06-19 15:24:18 +0800

    add dummy

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 export default {
   secretKey: process.env.SECRET_KEY || "customjirasecretkey",
   adminPassword: process.env.ADMIN_PASSWORD || "admin",
-  mailerUser: process.env.MAILER_USER || "vanbrixx@gmail.com",
+  mailerUser: process.env.MAILER_USER || "dummy@account.com",
   mailerPass: process.env.MAILER_PASS || "sendpulseaccountpassword"
 };
